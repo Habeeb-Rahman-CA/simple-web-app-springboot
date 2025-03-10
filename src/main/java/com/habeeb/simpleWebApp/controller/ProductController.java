b9c758b800f3c9244e77a22401f1683eb52c4ad8
Habeeb Rahman <habeebrahmanca07@gmail.com>
2025-03-10 18:15:24 +0530

    Rest API CRUD operation

--- src/main/java/com/habeeb/simpleWebApp/controller/ProductController.java
+++ src/main/java/com/habeeb/simpleWebApp/controller/ProductController.java
@@ -3,7 +3,12 @@ package com.habeeb.simpleWebApp.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.habeeb.simpleWebApp.model.Product;
@@ -12,12 +17,38 @@ import com.habeeb.simpleWebApp.service.ProductService;
 @RestController
 public class ProductController {
 
+    // Injecting the service
     @Autowired
     ProductService service;
 
-    @RequestMapping("/products")
+    // Get all product
+    @GetMapping("/products")
     public List<Product> getProducts() {
         return service.getProducts();
+    }
+
+    // Get product based on Product Id
+    @GetMapping("/products/{prodId}")
+    public Product getProductById(@PathVariable int prodId) {
+        return service.getProductById(prodId);
+    }
+
+    // Adding new product to the Product list
+    @PostMapping("/products")
+    public void addProduct(@RequestBody Product prod) {
+        service.addProduct(prod);
+    }
+
+    // Updating a product based on Product Id
+    @PutMapping("/products")
+    public void updateProduct(@RequestBody Product prod) {
+        service.updateProduct(prod);
+    }
+
+    // Delete a product based on Product Id
+    @DeleteMapping("/products/{prodId}")
+    public void deleteProduct(@PathVariable int prodId) {
+        service.deleteProduct(prodId);
     }
 
 }
--- src/main/java/com/habeeb/simpleWebApp/service/ProductService.java
+++ src/main/java/com/habeeb/simpleWebApp/service/ProductService.java
@@ -1,21 +1,58 @@
 package com.habeeb.simpleWebApp.service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.habeeb.simpleWebApp.model.Product;
 
 @Service
 public class ProductService {
 
-    List<Product> products = Arrays.asList(
+    // Dummy data
+    List<Product> products = new ArrayList<>(Arrays.asList(
             new Product(1, "MacBook Air", 50000),
             new Product(2, "IPhone 16e", 59999),
-            new Product(2, "AirPod 2nd Gen", 19999));
+            new Product(3, "AirPod 2nd Gen", 19999)));
 
+    // Get all product
     public List<Product> getProducts() {
         return products;
     }
+
+    // Get product based on Product Id
+    public Product getProductById(int prodId) {
+        return products.stream().filter(p -> p.getProdId() == prodId).findFirst().get();
+    }
+
+    // Adding new product to the Product list
+    public void addProduct(Product prod) {
+        products.add(prod);
+    }
+
+    // Updating a product based on Index
+    public void updateProduct(Product prod) {
+        int index = 0;
+        for (int i = 0; i < products.size(); i++) {
+            if (products.get(i).getProdId() == prod.getProdId()) {
+                index = i;
+            }
+        }
+        products.set(index, prod);
+    }
+
+    // Delete a product based on Product Id
+    public void deleteProduct(@PathVariable int prodId) {
+        int index = 0;
+        for (int i = 0; i < products.size(); i++) {
+            if (products.get(i).getProdId() == prodId) {
+                index = i;
+            }
+        }
+        products.remove(index);
+    }
+
 }
