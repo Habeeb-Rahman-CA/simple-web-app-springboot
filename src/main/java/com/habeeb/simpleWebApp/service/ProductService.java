66a9ef5dc09b61c67c14c6f5b10a835dff74528f
Habeeb Rahman <habeebrahmanca07@gmail.com>
2025-03-11 16:00:11 +0530

    removed unused imports

--- src/main/java/com/habeeb/simpleWebApp/service/ProductService.java
+++ src/main/java/com/habeeb/simpleWebApp/service/ProductService.java
@@ -1,13 +1,9 @@
 package com.habeeb.simpleWebApp.service;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
-
 import com.habeeb.simpleWebApp.model.Product;
 import com.habeeb.simpleWebApp.repository.ProductRepository;
 
